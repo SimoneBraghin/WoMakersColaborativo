159e4956bdeb94119f43dd7ba568fc1ee6609782
claricesoares <clarice.soares@ee.ufcg.edu.br>
2024-01-18 17:51:35 -0300

    Add trat. exc. q9

--- q9.par_impar.py
+++ q9.par_impar.py
@@ -3,18 +3,26 @@ par = []
 impar = []
 
 while aux != 0:
-    num = float(input("Digite um número: "))
     
-    if num % 2 == 0:
-        par.append(num)
-    else:
-        impar.append(num)
+    try:
     
-    aux = int(input("Deseja continuar? Tecle 0 para Encerrar ou 1 para continuar: "))
+        num = int(input("Digite um número: "))
+    
+        if num >= 0:
+            if num % 2 == 0:
+                par.append(num)
+            else:
+                impar.append(num)
+        else:
+            print("Por favor, digite um número positivo.")
+    
+        aux = int(input("Deseja continuar? Tecle 0 para Encerrar ou 1 para continuar: "))
+
+    except ValueError:
+        print("Por favor, digite um número inteiro positivo.")
+
 
 qnt_par = len(par)
 qnt_impar = len(impar)
 
 print(f'A quantidade números pares é {qnt_par} e quantidade de números ímpares é {qnt_impar}.')
-    
-
