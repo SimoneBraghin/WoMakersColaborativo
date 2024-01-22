6f14259dd7919122283bd197b33521272553bca1
SimoneBraghin <braghin.aai@gmail.com>
2024-01-22 15:14:24 -0300

    inclusão exercícios Isabela

---
+++ 04.FuncoesExtras/03.ConversaoTemperatura.py
@@ -0,0 +1,27 @@
+"""INSTRUÇÕES: Escreva um script que pergunta ao usuário se ele deseja converter uma temperatura de grau Celsius para Fahrenheit ou vice-versa. Para
+cada opção, crie uma função. Plus: Crie uma terceira, que é um menu para o usuário escolher a opção desejada, onde esse menu chama a função de 
+conversão correta."""
+
+# função que converte celsius para fahrenheit:
+def celsiusFahrenheit(t):
+    return t * 1.8 + 32
+
+# função que converte fahrenheit para celsius:
+def fahrenheitCelsius(t):
+    return (t -32) / 1.8 
+
+# Solicitando a escolha do tipo de conversão:
+unidade = input('Com relação à temperatura informada você gostaria de: (inserir o número da opção desejada)\n1. Converter de grau Celsius para Fahrenheit\n2. Converter de Fahrenheit para grau Celsius\n')
+
+# Laço para garantir que o usuário digite número válido:
+while unidade != '1' and unidade != '2':  
+    unidade = input('Valor inválido! Insira novamente o número da opção desejada:\n1. Converter de grau Celsius para Fahrenheit\n2. Converter de Fahrenheit para grau Celsius\n')
+
+# Solicitando ao usuário que entre com o valor da temperatura:
+temperatura = float(input('Digite a temperatura que você gostaria de converter: '))
+
+# Chamando aas funções de acordo com o valor informado pelo usuário e imprimindo o resultado:
+if unidade == '1':
+    print(f'Resultado: {celsiusFahrenheit(temperatura):.1f}F')
+else:
+    print(f'Resultado: {fahrenheitCelsius(temperatura):.1f}ºC')
--- README.md
+++ README.md
@@ -12,10 +12,15 @@ Repositório colaborativo do Squad Dorothy Vaughan
 #### 02. ReversoDoNumero
 1. Simone Braghin realizou na íntegra o Exercício 2.
 
-#### 03.
+#### 03. ConversaoTemperatura
+1. IsahBag resolveu o exercício na íntegra
+
 #### 04.
 1. Clarice Soares realizou na íntegra o Exercício 4.
+
 #### 05.
+1. IsahBag resolveu o exercício na íntegra
+
 #### 06. JogoDeForca
 1. Realizado pela JaneSSF
 2. Revisado por SimoneBraghin
